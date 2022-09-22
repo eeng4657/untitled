7d6247f818f5772269ffc896f2e2c1b63a219b2b
eeng4657 <eeng4657@bths.edu>
2022-09-21 22:07:11 -0400

    Added println statement for favorite food.

--- src/sample.java
+++ src/sample.java
@@ -1,6 +1,7 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("My name is Eric Eng.");
-        System.out.print("The Lighthouse");
+        System.out.println("The Lighthouse");
+        System.out.println("Pasta Carbonara");
     }
 }
